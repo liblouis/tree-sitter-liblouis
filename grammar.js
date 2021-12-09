ea559fc39c671e99ffd7c6d7a19c6ec69f763e16
Christian Egli <christian@egli.cc>
2021-12-09 17:22:35 +0100

    Handle most of the translation opcodes

--- grammar.js
+++ grammar.js
@@ -54,6 +54,35 @@ module.exports = grammar({
 	    $.numericnocontchars,
 	    $.numericmodechars,
 	    $.midendnumericmodechars,
+
+	    $.decpoint,
+	    $.hyphen, // FIXME: Does hyphen really need a nofor prefix?
+
+	    $.capsnocont,
+
+	    // Translation Opcodes
+	    $.compbrl,
+	    $.comp6,
+	    $.nocont,
+	    $.replace,
+	    $.always,
+	    $.repeated,
+	    $.repword,
+	    $.rependword,
+	    $.largesign,
+	    $.word,
+	    $.syllable,
+	    $.joinword,
+	    $.lowword,
+	    $.contraction,
+	    $.sufword,
+	    $.prfword,
+	    $.begword,
+	    $.begmidword,
+	    $.midword,
+	    $.midendword,
+	    $.endword,
+	    $.partword,
 ),
 
 	include: $ => seq('include', $._sp, $.filename),
@@ -91,6 +120,37 @@ module.exports = grammar({
 	numericmodechars: $ => seq('numericmodechars', $._sp, $.chars),
 	midendnumericmodechars: $ => seq('midendnumericmodechars', $._sp, $.chars),
 
+	decpoint: $ => seq('decpoint', $._sp, $.chars, $._sp, $.dots),
+	hyphen: $ => seq('hyphen', $._sp, $.chars, $._sp, $.dots),
+	capsnocont: $ => 'capsnocont',
+
+	compbrl: $ => seq(optional($._prefix), 'compbrl', $._sp, $.chars),
+	comp6: $ => seq('comp6', $._sp, $.chars, $._sp, $.dots_or_equal),
+	nocont: $ => seq('nocont', $._sp, $.chars),
+	replace: $ => choice(
+	    seq('replace', $._sp, $.chars),
+	    // prefer the longer rule
+	    prec(1, seq('replace', $._sp, $.chars, $._sp, $.chars))
+	),
+	always: $ => seq(optional($._prefix), repeat($.with_class), 'always', $._sp, $.chars, $._sp, $.dots_with_zero),
+	repeated: $ => seq(optional($._prefix), 'repeated', $._sp, $.chars, $._sp, $.dots_with_zero),
+	repword: $ => seq('repword', $._sp, $.chars, $._sp, $.dots),
+	rependword: $ => seq('rependword', $._sp, $.chars, $._sp, $.dots, ',', $.dots),
+	largesign: $ => seq('largesign', $._sp, $.chars, $._sp, $.dots),
+	word: $ => seq(optional($._prefix), repeat($.with_class), 'word', $._sp, $.chars, $._sp, $.dots),
+	syllable: $ => seq('syllable', $._sp, $.chars, $._sp, $.dots_or_equal),
+	joinword: $ => seq('joinword', $._sp, $.chars, $._sp, $.dots),
+	lowword: $ => seq(optional($._prefix), 'lowword', $._sp, $.chars, $._sp, $.dots),
+	contraction: $ => seq('contraction', $._sp, $.chars),
+	sufword: $ => seq(optional($._prefix), repeat($.with_class), 'sufword', $._sp, $.chars, $._sp, $.dots),
+	prfword: $ => seq('prfword', $._sp, $.chars, $._sp, $.dots),
+	begword: $ => seq(optional($._prefix), repeat($.with_class), 'begword', $._sp, $.chars, $._sp, $.dots_with_zero_or_equal),
+	begmidword: $ => seq(optional($._prefix), repeat($.with_class), 'begmidword', $._sp, $.chars, $._sp, $.dots),
+	midword: $ => seq(optional($._prefix), repeat($.with_class), 'midword', $._sp, $.chars, $._sp, $.dots_with_zero),
+	midendword: $ => seq(optional($._prefix), repeat($.with_class), 'midendword', $._sp, $.chars, $._sp, $.dots_or_equal),
+	endword: $ => seq(optional($._prefix), repeat($.with_class), 'endword', $._sp, $.chars, $._sp, $.dots_with_zero_or_equal),
+	partword: $ => seq(optional($._prefix), repeat($.with_class), 'partword', $._sp, $.chars, $._sp, $.dots),
+
 	before: $ => 'before',
 	after: $ => 'after',
 	noback: $ => 'noback',
@@ -104,8 +164,13 @@ module.exports = grammar({
 		$.nocross),
 	    $._sp
 	),
+	with_class: $ => seq(choice($.before, $.after), $._sp, $.ascii_chars, $._sp),
 
-	_dots_with_zero_or_equal: $ => choice(
+	dots_or_equal: $ => choice(
+	    $.equal,
+	    $.dots
+	),
+	dots_with_zero_or_equal: $ => choice(
 	    $.equal,
 	    $.dots_with_zero
 	),
@@ -117,6 +182,7 @@ module.exports = grammar({
 
 	esc_seq: $ => choice('\\\\', '\\f', '\\n', '\\r', '\\s', '\\t', '\\v', '\\e'),
 	chars: $ => choice($.esc_seq, /[^ \t\n]+/),
+	ascii_chars: $ => /[a-zA-Z][a-zA-Z0-9]*/,
 
 	filename: $ => /[-a-zA-Z0-9._\/]+/,
 	_newline: $ => choice('\n', '\r\n', '\r'),
